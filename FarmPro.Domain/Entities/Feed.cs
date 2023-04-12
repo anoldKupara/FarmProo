728d552e5cd7df7b452197ddc823b5b07c937b6a
Anold Kupara <kuparaanold20@gmail.com>
2023-04-13 01:07:03 +0200

    added Feed model class

---
+++ FarmPro.Domain/Entities/Feed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Domain.Entities
+{
+    public class Feed
+    {
+        public int FeedID { get; set; }
+        public string FeedType { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal Quantity { get; set; }
+        public string Comments { get; set; }
+        public virtual ICollection<FeedConsumption> FeedConsumptions { get; set; }
+    }
+}
