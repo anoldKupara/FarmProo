2e43f43ae1b665b1409ddb7f1bf3fdaeea1f857a
Anold Kupara <kuparaanold20@gmail.com>
2023-04-13 01:05:58 +0200

    added Animal model class

---
+++ FarmPro.Domain/Entities/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Domain.Entities
+{
+    public class Animal
+    {
+        public int AnimalID { get; set; }
+        public string AnimalType { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Comments { get; set; }
+        public virtual ICollection<Egg> Eggs { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+        public virtual ICollection<Mortality> Mortalities { get; set; }
+        public virtual ICollection<FeedConsumption> FeedConsumptions { get; set; }
+    }
+}
