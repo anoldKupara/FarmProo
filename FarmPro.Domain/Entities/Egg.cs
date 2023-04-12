12e56b6b05c3a13fab05140df7643d336e7cbf0a
Anold Kupara <kuparaanold20@gmail.com>
2023-04-13 01:08:51 +0200

    added Vaccine moel class

---
+++ FarmPro.Domain/Entities/Egg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Domain.Entities
+{
+    public class Egg
+    {
+        public int EggID { get; set; }
+        public int AnimalID { get; set; }
+        public DateTime LayDate { get; set; }
+        public string Comments { get; set; }
+        public virtual Animal Animal { get; set; }
+    }
+}
---
+++ FarmPro.Domain/Entities/Vaccine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Domain.Entities
+{
+    public class Vaccine
+    {
+        public int VaccineID { get; set; }
+        public string VaccineType { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string Comments { get; set; }
+    }
+}
