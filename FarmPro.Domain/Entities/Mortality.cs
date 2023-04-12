15a41501c4f1d885aadf768f0d2f0a97e5f0d7da
Anold Kupara <kuparaanold20@gmail.com>
2023-04-13 01:09:56 +0200

    added Mortality model class

---
+++ FarmPro.Domain/Entities/Mortality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Domain.Entities
+{
+    public class Mortality
+    {
+        public int MortalityID { get; set; }
+        public int AnimalID { get; set; }
+        public DateTime Date { get; set; }
+        public string Reason { get; set; }
+        public string Comments { get; set; }
+        public virtual Animal Animal { get; set; }
+    }
+}
