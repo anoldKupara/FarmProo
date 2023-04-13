db38c175bbfe728389c855327143c8494f17a76c
Anold Kupara <kuparaanold20@gmail.com>
2023-04-13 10:18:33 +0200

    added IAnimalRepository interface

---
+++ FarmPro.Application/Contracts/Infrastructure/IAnimalRepository.cs
@@ -0,0 +1,17 @@
+﻿using FarmPro.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Application.Contracts.Infrastructure
+{
+    public interface IAnimalRepository : IGenericRepository<Animal>
+    {
+        Task<IEnumerable<Animal>> GetAnimalsByBreed(int breedId);
+        Task<IEnumerable<Animal>> GetAnimalsByOwner(int ownerId);
+    }
+    {
+    }
+}
