9a35667af6c458f0c40a7b4fb1813f3cc3a5e5e5
Anold Kupara <kuparaanold20@gmail.com>
2023-04-14 14:42:35 +0200

    added SaleDto

---
+++ FarmPro.Application/DTOs/Sales/SaleDto.cs
@@ -0,0 +1,19 @@
+﻿using FarmPro.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Application.DTOs.Sales
+{
+    public class SaleDto
+    {
+        public int SaleID { get; set; }
+        public int AnimalID { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal SalePrice { get; set; }
+        public string Comments { get; set; }
+        public virtual Animal Animal { get; set; }
+    }
+}
