654156b4dd56ccad7a66efb7dc14e8dba18749d0
Anold Kupara <kuparaanold20@gmail.com>
2023-04-14 15:10:33 +0200

    added AnimalListDto class

---
+++ FarmPro.Application/DTOs/Animals/AnimalListDto.cs
@@ -0,0 +1,26 @@
+﻿using FarmPro.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmPro.Application.DTOs.Animals
+{
+    public class AnimalListDto
+    {
+        public int AnimalID { get; set; }
+        public string AnimalType { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Comments { get; set; }
+        public virtual ICollection<Egg> Eggs { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+        public virtual ICollection<Mortality> Mortalities { get; set; }
+        public virtual ICollection<FeedConsumption> FeedConsumptions { get; set; }
+    }
+}
